0ef7455afde8fbc2a12251b6b22b2ff046642eb2
Sander van der Burg <svanderburg@gmail.com>
2019-09-01 16:36:02 +0200

    Add capacity as machine-level property

--- deployment/configurations/test-vm.nix
+++ deployment/configurations/test-vm.nix
@@ -5,15 +5,19 @@
   networking.firewall.allowedTCPPorts = [ 80 ];
   services.disnix.enable = true;
   services.openssh.enable = true;
-  
+
+  dysnomia.properties = {
+    capacity = 10;
+  };
+
   environment.systemPackages = [
     pkgs.w3m
   ];
-  
+
   users.extraGroups = {
     nginx = { gid = 60; };
   };
-  
+
   users.extraUsers = {
     nginx = { group = "nginx"; uid = 60; };
   };
